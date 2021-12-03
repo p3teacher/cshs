0edefa0866f6b2efd15b193cce5d5c9e0e9642d8
Jackie Lin <72183020+p3teacher@users.noreply.github.com>
2021-12-04 06:28:17 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+# hello.py
+# run this code in File> New> Terminal
+# after prompt INPUT : python hello.py
+import sys
+major_version = sys.version_info.major
+minor_version = sys.version_info.minor
+print("Hello from Binder Python {}.{}!".format(major_version, minor_version))
